adead9c1421944caf5c67f7632df6b8640cc94cb
Ubuntu <nicklerma@Ubuntu16.25yqxvcmfahurfjjtis23dljwf.gx.internal.cloudapp.net>
2020-04-03 21:33:48 +0000

    Added Perl file to the directory

---
+++ Perl/elseif.pl
@@ -0,0 +1,13 @@
+my $a=5;
+if($a==6)
+{
+    print "Executed If block -- The value is $a";
+}
+elsif($a==5)
+{
+    print "Executed elsif block --The value is $a";
+}
+else
+{
+    print "Executed else block – The value is $a";
+}
